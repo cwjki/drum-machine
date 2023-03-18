1d6cf2ddf7eb9eb7b5151b893b5c345102b26104
cwjki <cwjki97@gmail.com>
2023-03-18 19:26:46 -0400

    created the drum box, need to stylish more

--- src/components/DrumBox.tsx
+++ src/components/DrumBox.tsx
@@ -1,48 +1,42 @@
 const DrumBox = () => {
   return (
-    <div className="container-fluid vh-100 vw-100 bg-primary">
-      <div className="row align-items-center vh-100 vw-100">
-        <div className="col-md-6 col-sm-8 col-12 mx-auto">
-          <div className="card shadow bg-secondary rounded-3">
-            <div className="container-fluid p-5 border text-center">
-              <div className="row align-items-center bg-danger m-1">
-                <div className="col-4">
-                  <button className="btn text-light border p-4">Q</button>
-                </div>
-                <div className="col-4">
-                  <button className="btn text-light border p-4">W</button>
-                </div>
-                <div className="col-4">
-                  <button className="btn text-light border p-4">E</button>
-                </div>
-                
-              </div>
-              <div className="row align-items-center">
-                <div className="col-4">
-                  <button>Q</button>
-                </div>
-                <div className="col-4">
-                  <button>W</button>
-                </div>
-                <div className="col-4">
-                  <button>E</button>
-                </div>
-                
-              </div>
-              <div className="row align-items-center">
-                <div className="col-4">
-                  <button>Q</button>
-                </div>
-                <div className="col-4">
-                  <button>W</button>
-                </div>
-                <div className="col-4">
-                  <button>E</button>
-                </div>
-                
-              </div>
+    <div className="container-fluid bg-primary ">
+      <div className="row align-items-center min-vw-100 min-vh-100">
+        <div className="col-md-6 col-sm-9 col-11 mx-auto border p-4 bg-secondary rounded-4 shadow-lg">
+          <div className="row text-center mt-1">
+            <div className="col mybtn">
+              <button className="btn btn-info fs-2 fw-bold px-4">Q</button>
+            </div>
+            <div className="col">
+              <button className="btn btn-info fs-2 fw-bold px-4">W</button>
+            </div>
+            <div className="col">
+              <button className="btn btn-info fs-2 fw-bold px-4">E</button>
             </div>
           </div>
+          <div className="row text-center mt-1">
+            <div className="col">
+              <button className="btn btn-info fs-2 fw-bold">A</button>
+            </div>
+            <div className="col">
+              <button className="btn btn-info fs-2 fw-bold">S</button>
+            </div>
+            <div className="col">
+              <button className="btn btn-info fs-2 fw-bold">D</button>
+            </div>
+          </div>
+          <div className="row text-center mt-1">
+            <div className="col">
+              <button className="btn btn-info fs-2 fw-bold">Z</button>
+            </div>
+            <div className="col">
+              <button className="btn btn-info fs-2 fw-bold">X</button>
+            </div>
+            <div className="col">
+              <button className="btn btn-info fs-2 fw-bold">C</button>
+            </div>
+          </div>
+
         </div>
       </div>
     </div>
