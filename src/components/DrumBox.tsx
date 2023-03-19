a4e5854fb7eebc9afd84344d621302ab4e2946d8
cwjki <cwjki97@gmail.com>
2023-03-19 13:42:04 -0400

    added the sound card

--- src/components/DrumBox.tsx
+++ src/components/DrumBox.tsx
@@ -5,9 +5,12 @@ const DrumBox = () => {
         <div className="col-md-10 col-12 mx-auto">
           <div className="card bg-secondary rounded-4 shadow-lg">
             <div className="card-body ">
-              <div className="card-title text-info fs-2 fw-bold">Drum Machine</div>
+              <div className="card-title text-info fs-2 fw-bold">
+                Drum Machine
+              </div>
               <div className="card-text text-primary fw-semibold">
-                the third project of the freeCodeCamp's Front End Development Libraries certification
+                the third project of the freeCodeCamp's Front End Development
+                Libraries certification
               </div>
             </div>
           </div>
@@ -16,7 +19,14 @@ const DrumBox = () => {
 
       <div className="row align-items-center mt-5 pt-4">
         <div className="col-md-6 col-sm-9 col-11 mx-auto border border-info p-5 bg-secondary rounded-4 shadow-lg">
-          <div className="row text-center mt-1">
+          <div className="row align-items-center text-center">
+            <div className="col-6 mx-auto">
+              <div className="card bg-primary rounded-4">
+                <div className="fs-6 py-1">Sound</div>
+              </div>
+            </div>
+          </div>
+          <div className="row text-center mt-4">
             <div className="col mybtn">
               <button className="btn btn-info fs-2 fw-bold">Q</button>
             </div>
